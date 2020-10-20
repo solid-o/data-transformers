6e3b248552d9d6a6772bcc40c1567c34b588d8b4
Alessandro Chitolina <alekitto@gmail.com>
2020-10-20 09:44:58 +0200

    add urn to item transformer

---
+++ src/Transformer/UrnToItemTransformer.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Solido\DataTransformers\Transformer;
+
+use Solido\Common\Exception\ResourceNotFoundException;
+use Solido\Common\Urn\Urn;
+use Solido\Common\Urn\UrnConverterInterface;
+use Solido\DataTransformers\Exception\TransformationFailedException;
+use Solido\DataTransformers\TransformerInterface;
+use function get_class;
+use function gettype;
+use function is_object;
+use function is_string;
+
+class UrnToItemTransformer implements TransformerInterface
+{
+    private UrnConverterInterface $urnConverter;
+
+    public function __construct(UrnConverterInterface $urnConverter)
+    {
+        $this->urnConverter = $urnConverter;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($value, ?string $acceptable = null): ?object
+    {
+        if ($value === null || $value === '') {
+            return null;
+        }
+
+        if (is_object($value) && ! $value instanceof Urn) {
+            return $value;
+        }
+
+        if (! is_string($value) && ! $value instanceof Urn) {
+            throw new TransformationFailedException('Expected a string. Got ' . (is_object($value) ? get_class($value) : gettype($value)));
+        }
+
+        $urn = new Urn($value);
+
+        try {
+            return $this->urnConverter->getItemFromUrn($urn, $acceptable);
+        } catch (ResourceNotFoundException $e) {
+            throw new TransformationFailedException('Could not find the desired object with ID ' . $urn->id, 0, $e);
+        }
+    }
+}
---
+++ tests/Transformer/UrnToItemTransformerTest.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+namespace Solido\DataTransformers\Tests\Transformer;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\ObjectProphecy;
+use Solido\Common\Exception\ResourceNotFoundException;
+use Solido\Common\Urn\Urn;
+use Solido\Common\Urn\UrnConverterInterface;
+use Solido\DataTransformers\Exception\TransformationFailedException;
+use Solido\DataTransformers\Transformer\ChainTransformer;
+use Solido\DataTransformers\Transformer\UrnToItemTransformer;
+use Solido\DataTransformers\TransformerInterface;
+
+class UrnToItemTransformerTest extends TestCase
+{
+    use ProphecyTrait;
+
+    /**
+     * @var UrnConverterInterface|ObjectProphecy
+     */
+    private ObjectProphecy $urnConverter;
+
+    private UrnToItemTransformer $transformer;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        $this->urnConverter = $this->prophesize(UrnConverterInterface::class);
+        $this->transformer = new UrnToItemTransformer($this->urnConverter->reveal());
+    }
+
+    public function testShouldTransformToNull(): void
+    {
+        self::assertNull($this->transformer->transform(''));
+        self::assertNull($this->transformer->transform(null));
+    }
+
+    public function testShouldNotTransformIfObjectIsPassed(): void
+    {
+        $obj = new \stdClass();
+
+        self::assertSame($obj, $this->transformer->transform($obj));
+    }
+
+    public function provideNotUrn(): iterable
+    {
+        yield [ true ];
+        yield [ 42 ];
+        yield [ [] ];
+        yield [ \fopen('php://temp', 'rb') ];
+    }
+
+    /**
+     * @dataProvider provideNotUrn
+     */
+    public function testShouldThrowIfNotUrnIsPassed($value): void
+    {
+        $this->expectException(TransformationFailedException::class);
+
+        $this->transformer->transform($value);
+    }
+
+    public function testShouldTransformToObject(): void
+    {
+        $obj = new \stdClass();
+        $this->urnConverter->getItemFromUrn(new Urn('test'), null)->willReturn($obj);
+
+        self::assertSame($obj, $this->transformer->transform('test'));
+        self::assertSame($obj, $this->transformer->transform(new Urn('test')));
+    }
+
+    public function testShouldFailIfNotFound(): void
+    {
+        $this->expectException(TransformationFailedException::class);
+        $this->urnConverter->getItemFromUrn(new Urn('test'), null)
+            ->willThrow(ResourceNotFoundException::class);
+
+        $this->transformer->transform('test');
+    }
+}
